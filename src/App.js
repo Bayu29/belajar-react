2cf69c0e324b3ec86e8a7ae2195ef05c0502693c
Bayu29 <bsegara2903@gmail.com>
2021-12-30 13:21:05 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import ProductList from "./component/ProductList"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  //const [title, setTitle] = useState('Hello World');
+
+  // const clickMe = (name) => {
+  //   console.log('Halo : ' + name);
+  // }
+
+  // const changeTitle = () => {
+  //   setTitle("Title changed");
+  // }
+
+  const [products, setProducts] = useState([
+    { id: 1, title: "Products 1", price: 899 },
+    { id: 2, title: "Products 2", price: 901 },
+    { id: 3, title: "Products 3", price: 599 },
+    { id: 4, title: "Products 4", price: 699 },
+    { id: 5, title: "Products 5", price: 799 },
+  ]);
+
+  const [name, setName] = useState('Bayu');
+
+  // return (
+  //   <div>
+  //     <Header/>
+  //     <button onClick = { () => clickMe('Bayu') } >Click Me</button>
+  //   </div>
+  // );
+
+  // return (
+  //   <div>
+  //    <h1>{ title }</h1>
+  //    <button onClick={ changeTitle }>Change Title</button>
+  //   </div>
+  // )
+
+  const deleteProduct = (productId) => {
+    const newProducts = products.filter(product => product.id !== productId);
+    setProducts(newProducts);
+  }
+
+  useEffect(() => {
+    console.log('Use Effect Running');    
+  }, [name]);
+
+  return  (
+    <div>
+     <ProductList products={ products } deleteProduct={ deleteProduct }/>
+     <button onClick={ () => setName('Jhon') }>Change Name</button>
+     <p>Name: { name } </p>
     </div>
-  );
+  )
 }
 
 export default App;
